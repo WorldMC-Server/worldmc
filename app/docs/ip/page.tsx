b88bc70e1357f16d5f2e3fb4a661ad08cc78eb5c
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-09-19 19:33:49 +1000

    stop weird glitch by ensuring 4xl

--- app/docs/ip/page.tsx
+++ app/docs/ip/page.tsx
@@ -4,16 +4,9 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <section className="container w-fit space-y-4">
+    <section className="container w-full max-w-4xl space-y-4">
       <h1 className="text-5xl font-black">Joining WorldMC</h1>
-      <Image
-        src="/worldmap.png"
-        alt="WorldMC Earth Map"
-        className="max-w-4xl rounded-box shadow"
-        width={1920}
-        height={1080}
-        layout="responsive"
-      />
+      <Image src="/worldmap.png" alt="WorldMC Earth Map" className="rounded-box shadow" width={1920} height={1080} layout="responsive" />
       <ol className="list-inside list-decimal">
         <li>Open Minecraft and go to Multiplayer</li>
         <li>Click &quot;Add Server&quot; or &quot;Direct Connect&quot;</li>
