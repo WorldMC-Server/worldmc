afdd2ba68c77ba4cec39a47761fcd844bbd0f521
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-09-22 00:43:27 +1000

    change recipes

--- app/docs/recipes/recipes.ts
+++ app/docs/recipes/recipes.ts
@@ -2,36 +2,6 @@ import { MinecraftRecipeProps } from "./components/MinecraftRecipe";
 
 const recipes: MinecraftRecipeProps[] = [
   {
-    title: "End Stone",
-    ingredients: [
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/cobblestone.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-      "/minecraft/block/sand.png",
-    ],
-    result: "/minecraft/block/end_stone.png",
-  },
-  {
-    title: "Prismarine Shard",
-    ingredients: [
-      null,
-      null,
-      null,
-      "/minecraft/block/sand.png",
-      "/minecraft/block/dirt.png",
-      "/minecraft/item/lapis_lazuli.png",
-      null,
-      null,
-      null,
-    ],
-    result: "/minecraft/item/prismarine_shard.png",
-  },
-  {
     title: "Prismarine Crystals",
     ingredients: [null, null, null, null, "/minecraft/item/prismarine_shard.png", "/minecraft/item/glowstone_dust.png", null, null, null],
     result: "/minecraft/item/prismarine_crystals.png",
@@ -65,21 +35,6 @@ const recipes: MinecraftRecipeProps[] = [
       "/minecraft/item/iron_ingot.png",
     ],
     result: "/minecraft/block/powered_rail.png",
-  },
-  {
-    title: "Shulker Shell",
-    ingredients: [
-      "/minecraft/block/amethyst_block.png",
-      "/minecraft/block/amethyst_block.png",
-      "/minecraft/block/amethyst_block.png",
-      "/minecraft/block/amethyst_block.png",
-      "/minecraft/item/phantom_membrane.png",
-      "/minecraft/block/amethyst_block.png",
-      "/minecraft/item/amethyst_shard.png",
-      "/minecraft/item/amethyst_shard.png",
-      "/minecraft/item/amethyst_shard.png",
-    ],
-    result: "/minecraft/item/shulker_shell.png",
   },
 ];
 
