aebaf8c6cf0741ba5ae965386516dbecc29c74c3
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-09-24 19:22:58 +1000

    new format utils

--- lib/format.ts
+++ lib/format.ts
@@ -8,3 +8,15 @@ export function formatDateTime(epochDate: number) {
   };
   return date.toLocaleString(undefined, options);
 }
+
+export function replaceUnderscoresWithSpaces(input: string) {
+  return input.replace(/_/g, " ");
+}
+
+export function convertToTitleCase(input: string): string {
+  return input
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
