a8e4e928ee312da737af1ff4d9afe5dae16b66fe
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-09-25 18:52:49 +1000

    display num residents on nation button

--- components/towny/NationButton.tsx
+++ components/towny/NationButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Flag } from "lucide-react";
+import { Flag, Users } from "lucide-react";
 import { replaceUnderscoresWithSpaces } from "@/lib/format";
 import { PartialNation } from "@/types/bridge";
 
@@ -13,6 +13,10 @@ export default function NationButton({ item }: NationButtonProps) {
     <Link href={`/nations/${item.UUID}`} className="btn btn-lg btn-block justify-between shadow">
       <div className="flex min-w-0 flex-1 items-center gap-4">
         <span className="truncate leading-normal">{replaceUnderscoresWithSpaces(item.name)}</span>
+        <div className="badge badge-lg whitespace-nowrap font-normal">
+          <Users className="mr-1 size-4" />
+          {item.numResidents}
+        </div>
       </div>
       <Flag />
     </Link>
