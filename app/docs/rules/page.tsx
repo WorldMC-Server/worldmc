c86e317e332d9c17c2a26f546d0a3df4ca6256f4
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-10-06 19:05:36 +1000

    separate rules data from page + metadata

--- app/docs/rules/page.tsx
+++ app/docs/rules/page.tsx
@@ -1,39 +1,13 @@
 import React from "react";
 import { Book } from "lucide-react";
+import { Metadata } from "next";
+import { ruleCategories } from "@/app/docs/rules/rules";
+
+export const metadata: Metadata = {
+  title: "Rules",
+};
 
 export default function RulesPage() {
-  const ruleCategories = [
-    {
-      title: "Cheating and Exploits",
-      rules: [
-        "Cheat clients and unfair modifications are prohibited.",
-        "Exploiting bugs or glitches is not allowed.",
-        "Alternative accounts are forbidden.",
-        "Intentionally causing lag or disrupting server services is banned.",
-      ],
-    },
-    {
-      title: "Behavior and Chat",
-      rules: [
-        "Use appropriate names for nations, towns, and player accounts.",
-        "No spamming or disruptive behavior in chat.",
-        "Bullying, hate speech, and explicit content are prohibited.",
-        "Do not impersonate other players or entities.",
-        "Respect others' personal information and privacy.",
-        "Use English in global chat channels.",
-      ],
-    },
-    {
-      title: "Griefing and Land Management",
-      rules: [
-        "Griefing and stealing within your own town or nation is not allowed.",
-        "Do not damage land within or around claims.",
-        "Major terraforming that alters the world map is not allowed.",
-        "Building map art is not permitted.",
-      ],
-    },
-  ];
-
   const getMarkerContent = (categoryIndex: number, ruleIndex: number) => {
     return `${categoryIndex + 1}.${ruleIndex + 1}`;
   };
