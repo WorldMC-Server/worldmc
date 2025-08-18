8fb23c56eed206a8daa90629b011f302c4e4a9a9
Zeke Pari <pariezekiel@gmail.com>
2025-08-18 17:55:51 +1000

    update iframe source and link to direct play URL

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
     <div className="container space-y-4">
       <h1 className="text-center text-5xl font-black">Earth Map</h1>
       <div>
-        <iframe src="https://map.worldmc.net" className="aspect-[3/4] w-full rounded-box shadow sm:aspect-video" />
+        <iframe src="http://play.worldmc.net:8123/" className="aspect-[3/4] w-full rounded-box shadow sm:aspect-video" />
         <div className="flex flex-col justify-between text-left sm:flex-row">
           <div>
-            <Link href="https://map.worldmc.net" className="link-hover link-primary" target="_blank">
+            <Link href="http://play.worldmc.net:8123/" className="link-hover link-primary" target="_blank">
               View Full Map <ExternalLink className="inline-block size-4" />
             </Link>
             <p>Earth 1:1000</p>
